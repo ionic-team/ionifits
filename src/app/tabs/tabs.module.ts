204e26e8d975e7018f04a54d8c2c5300b4305b72
Ely Lucas <ely@meta-tek.net>
2019-01-07 15:11:45 -0700

    Initial commit

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TabsPageRoutingModule } from './tabs.router.module';
+
+import { TabsPage } from './tabs.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
