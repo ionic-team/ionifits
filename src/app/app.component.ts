3a14963fd53577d663faa13822a8765edcf6a467
Matt Netkow <matt.netkow@gmail.com>
2021-02-26 13:09:04 -0600

    Beta 3 updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
           for our users, hide the splash screen automatically 
           when the app is ready to be used:
           
-          https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
+          https://capacitorjs.com/docs/apis/splash-screen#hiding-the-splash-screen
       */
       SplashScreen.hide();
     }
