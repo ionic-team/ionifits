813ac6e53ac75b4e57af15e7c8eb6eb8f7b36f8f
Matt Netkow <matt.netkow@gmail.com>
2023-11-22 11:46:08 -0600

    Migrate to ESbuild from webpack

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.demoapp.ionifits',
   appName: 'Ionifits',
-  webDir: 'www',
+  webDir: 'www/browser',
   plugins: {
     SplashScreen: {
       launchShowDuration: 1000,
