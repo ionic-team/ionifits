1e16123b994960dfce9438d97f6912638893f093
Devin Shoemaker <devinshoe@gmail.com>
2023-12-07 13:52:34 -0600

    Switch to Cordova plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,16 +8,6 @@ const config: CapacitorConfig = {
     SplashScreen: {
       launchShowDuration: 1000,
       launchAutoHide: false
-    },
-    LiveUpdates: {
-      appId: '34df4710',
-      channel: 'new-lu-sdk',
-      autoUpdateMethod: 'background',
-      maxVersions: 3,
-      /* Disable Live Updates since this is a public reference app. 
-        Otherwise a live update override could cause confusion for developers.  */
-      // @ts-ignore until Capacitor 6 resolves this
-      enabled: false
     }
   }
 };
